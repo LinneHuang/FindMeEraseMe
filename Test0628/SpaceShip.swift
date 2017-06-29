69788aad687cd681ff43775aa366bea5be9b91f4
Linne S <linnnh@gmail.com>
2017-06-28 22:00:16 -0700

    initial commit

---
+++ Test0628/SpaceShip.swift
@@ -0,0 +1,27 @@
+//
+//  SpaceShip.swift
+//  Test0628
+//
+//  Created by Linne S. Huang on 6/28/17.
+//  Copyright © 2017 Linne S. Huang. All rights reserved.
+//
+
+import ARKit // Change it from UIKit to ARKit. Because we are using ARKit!
+
+class SpaceShip: SCNNode {
+    
+    func loadModal() {
+        guard let virtualObjectScene = SCNScene(named: "art.scnassets/ship.scn") else {return}
+        
+        let wrapperNode = SCNNode()
+        
+        for child in virtualObjectScene.rootNode.childNodes {
+            wrapperNode.addChildNode(child)
+        }
+        
+        self.addChildNode(wrapperNode)
+    }
+    
+    
+
+}
